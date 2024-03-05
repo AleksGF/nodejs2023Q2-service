a5c91cbab23ba995c0b51cf18614339065437399
AleksGF <aleksgf@mail.zp.ua>
2024-03-05 11:26:59 +0200

    feat: add dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 
-bootstrap();
+bootstrap().then(() => console.log(`Server started on port ${PORT}`));
