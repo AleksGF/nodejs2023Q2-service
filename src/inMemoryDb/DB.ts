625e38814ecb1b6195be64026b68a158d19d073b
AleksGF <aleksgf@mail.zp.ua>
2024-03-06 16:24:37 +0200

    feat: add db services

---
+++ src/inMemoryDb/DB.ts
@@ -0,0 +1,173 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Album, Artist, Favorites, Track, User } from '../database/types/';
+
+interface dataDB {
+  users: User[];
+  artists: Artist[];
+  tracks: Track[];
+  albums: Album[];
+  favorites: Favorites[];
+}
+
+export class DB {
+  private _db: dataDB | null;
+
+  async $findMany(modelName: string) {
+    const model = this.$getModel(modelName);
+
+    return [...model];
+  }
+
+  async $findUnique(
+    modelName: string,
+    { where }: { where: Record<string, any> },
+  ) {
+    const model = this.$getModel(modelName);
+
+    return (
+      model.find((item: Record<string, any>) => {
+        for (const key in where) {
+          if (item[key] !== where[key]) {
+            return false;
+          }
+        }
+
+        return true;
+      }) ?? null
+    );
+  }
+
+  async $create(modelName: string, data: Record<string, any>) {
+    const model = this.$getModel(modelName);
+
+    const id = uuidv4();
+    const version = 1;
+    const createdAt = Date.now();
+    const updatedAt = Date.now();
+
+    if (modelName === 'users') {
+      const user = {
+        ...(data as { login: string; password: string }),
+        id,
+        version,
+        createdAt,
+        updatedAt,
+      };
+      model.push(user);
+
+      return user;
+    }
+  }
+
+  async $update(
+    modelName: string,
+    { where, data }: { where: Record<string, any>; data: Record<string, any> },
+  ) {
+    const model = this.$getModel(modelName);
+
+    const index = model.findIndex((item: Record<string, any>) => {
+      for (const key in where) {
+        if (item[key] !== where[key]) return false;
+      }
+
+      return true;
+    });
+
+    if (index === -1) return null;
+
+    const updatedItem = {
+      ...model[index],
+      ...data,
+      version: model[index].version + 1,
+      updatedAt: Date.now(),
+    };
+
+    model[index] = updatedItem;
+
+    return updatedItem;
+  }
+
+  async $delete(modelName: string, { where }: { where: Record<string, any> }) {
+    const model = this.$getModel(modelName);
+
+    const index = model.findIndex((item: Record<string, any>) => {
+      for (const key in where) {
+        if (item[key] !== where[key]) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+
+    if (index === -1) return null;
+
+    return model.splice(index, 1)[0];
+  }
+
+  private $checkConnection() {
+    if (!this._db) {
+      throw new Error('Database not connected');
+    }
+  }
+
+  private $getModel(modelName: string) {
+    this.$checkConnection();
+    const model = this._db[modelName];
+
+    if (!model) {
+      throw new Error(`Model '${modelName}' not found`);
+    }
+
+    return model;
+  }
+
+  private $generateMethods(modelName: string) {
+    return {
+      findMany: () => this.$findMany(modelName),
+      findUnique: (data: { where: Record<string, any> }) =>
+        this.$findUnique(modelName, data),
+      create: (data: Record<string, any>) => this.$create(modelName, data),
+      update: (data: {
+        where: Record<string, any>;
+        data: Record<string, any>;
+      }) => this.$update(modelName, data),
+      delete: (data: { where: Record<string, any> }) =>
+        this.$delete(modelName, data),
+    };
+  }
+
+  get user() {
+    return this.$generateMethods('users');
+  }
+
+  get artist() {
+    return this.$generateMethods('artists');
+  }
+
+  get track() {
+    return this.$generateMethods('tracks');
+  }
+
+  get album() {
+    return this.$generateMethods('albums');
+  }
+
+  get favorite() {
+    return this.$generateMethods('favorites');
+  }
+
+  async $connect() {
+    this._db = {
+      users: [],
+      artists: [],
+      tracks: [],
+      albums: [],
+      favorites: [],
+    };
+  }
+
+  async $disconnect() {
+    this._db = null;
+  }
+}
