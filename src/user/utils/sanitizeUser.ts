0ef5b3ec79c5a68b567dd5daf6240394a86df9b8
AleksGF <aleksgf@mail.zp.ua>
2024-03-06 16:28:20 +0200

    feat: add user module

---
+++ src/user/utils/sanitizeUser.ts
@@ -0,0 +1,9 @@
+import { User } from '../user.interface';
+
+export const sanitizeUser = (user: User): Omit<User, 'password'> => {
+  if (!user || typeof user !== 'object' || !('password' in user)) return user;
+
+  const { password, ...sanitizedUser } = user;
+
+  return sanitizedUser;
+};
