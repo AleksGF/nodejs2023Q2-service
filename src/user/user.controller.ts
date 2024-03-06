1e84f0fb2e60f33866659923916f147f1f3f2fda
AleksGF <aleksgf@mail.zp.ua>
2024-03-06 16:28:20 +0200

    feat: add user module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto.dto';
+import { UpdatePasswordDto } from './dto/updatePasswordDto.dto';
+import { User } from './user.interface';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUsers(): Promise<Omit<User, 'password'>[]> {
+    return this.userService.getAllUsers();
+  }
+
+  @Post()
+  createUser(
+    @Body(new ValidationPipe()) dto: CreateUserDto,
+  ): Promise<Omit<User, 'password'>> {
+    return this.userService.createUser(dto);
+  }
+
+  @Get(':id')
+  getUser(
+    @Param('id', new ParseUUIDPipe()) id: string,
+  ): Promise<Omit<User, 'password'>> {
+    return this.userService.getUserById(id);
+  }
+
+  @Put(':id')
+  updateUserPassword(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body(new ValidationPipe()) dto: UpdatePasswordDto,
+  ): Promise<Omit<User, 'password'>> {
+    return this.userService.updateUserPassword(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteUser(@Param('id', new ParseUUIDPipe()) id: string): Promise<any> {
+    return this.userService.deleteUser(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,83 @@
+import { compare, hash } from 'bcrypt';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { CreateUserDto } from './dto/createUserDto.dto';
+import { UpdatePasswordDto } from './dto/updatePasswordDto.dto';
+import { User } from './user.interface';
+import { sanitizeUser } from './utils/sanitizeUser';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async getAllUsers(): Promise<Omit<User, 'password'>[]> {
+    const users = await this.databaseService.user.findMany();
+
+    return users.map((user) => sanitizeUser(user));
+  }
+
+  async getUserById(id: string): Promise<Omit<User, 'password'>> {
+    const user = await this.databaseService.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User with id: ${id} not found`);
+    }
+
+    return sanitizeUser(user);
+  }
+
+  async createUser(dto: CreateUserDto): Promise<Omit<User, 'password'>> {
+    const CRYPT_SALT = Number(process.env.CRYPT_SALT) || 10;
+
+    const hashedPassword = await hash(dto.password, CRYPT_SALT);
+
+    const user = await this.databaseService.user.create({
+      ...dto,
+      password: hashedPassword,
+    });
+
+    return sanitizeUser(user);
+  }
+
+  async deleteUser(id: string): Promise<User> {
+    const user = await this.databaseService.user.delete({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User with id: ${id} not found`);
+    }
+
+    return user;
+  }
+
+  async updateUserPassword(
+    id: string,
+    dto: UpdatePasswordDto,
+  ): Promise<Omit<User, 'password'>> {
+    const user = await this.databaseService.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User with id: ${id} not found`);
+    }
+
+    const isPasswordMatch = await compare(dto.oldPassword, user.password);
+
+    if (!isPasswordMatch) {
+      throw new ForbiddenException('Current password is incorrect');
+    }
+
+    const CRYPT_SALT = Number(process.env.CRYPT_SALT) || 10;
+
+    const hashedPassword = await hash(dto.newPassword, CRYPT_SALT);
+
+    const updatedUser = await this.databaseService.user.update({
+      where: { id },
+      data: { password: hashedPassword },
+    });
+
+    return sanitizeUser(updatedUser);
+  }
+}
