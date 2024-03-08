a215c1cff845c309e8c4363ae4d7b75d73ef8fa2
AleksGF <aleksgf@mail.zp.ua>
2024-03-08 15:12:25 +0200

    feat: add album module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { AlbumModule } from './modules/album/track.module';
 import { ArtistModule } from './modules/artist/artist.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { TrackModule } from './modules/track/track.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, ArtistModule, TrackModule],
+  imports: [DatabaseModule, UserModule, ArtistModule, TrackModule, AlbumModule],
   providers: [
     {
       provide: APP_PIPE,
---
+++ src/modules/album/album.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateAlbumDto } from './dto/createAlbum.dto';
+import { UpdateAlbumDto } from './dto/updateAlbum.dto';
+import { Album } from './album.interface';
+import { AlbumService } from './album.service';
+
+@Controller('album')
+export class AlbumController {
+  constructor(private readonly albumService: AlbumService) {}
+
+  @Get()
+  getAlbums(): Promise<Album[]> {
+    return this.albumService.getAllAlbums();
+  }
+
+  @Post()
+  createAlbum(
+    @Body(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    )
+    dto: CreateAlbumDto,
+  ): Promise<Album> {
+    return this.albumService.createAlbum(dto);
+  }
+
+  @Get(':id')
+  getAlbum(@Param('id', new ParseUUIDPipe()) id: string): Promise<Album> {
+    return this.albumService.getAlbumById(id);
+  }
+
+  @Put(':id')
+  updateAlbum(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    )
+    dto: UpdateAlbumDto,
+  ): Promise<Album> {
+    return this.albumService.updateAlbum(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteAlbum(@Param('id', new ParseUUIDPipe()) id: string): Promise<Album> {
+    return this.albumService.deleteAlbum(id);
+  }
+}
