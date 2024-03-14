06bf649488e3000b20f7a5afac64ae0a4c1e775f
AleksGF <aleksgf@mail.zp.ua>
2024-03-14 18:13:53 +0200

    feat: add Prisma as DatabaseService

--- src/inMemoryDb/types/index.ts
+++
@@ -1,7 +0,0 @@
-import { Album } from './albumTypes';
-import { Artist } from './artistTypes';
-import { Favorites } from './favoritesTypes';
-import { Track } from './trackTypes';
-import { User } from './userTypes';
-
-export { User, Artist, Track, Album, Favorites };
