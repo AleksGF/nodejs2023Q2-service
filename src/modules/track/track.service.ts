9cee919da203cc0082ec34ac7f18b24fca5ae7fd
AleksGF <aleksgf@mail.zp.ua>
2024-03-17 15:47:12 +0200

    refactor: change track services for Prisma

--- src/modules/track/track.service.ts
+++ src/modules/track/track.service.ts
@@ -2,17 +2,18 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
 import { CreateTrackDto } from './dto/createTrack.dto';
 import { UpdateTrackDto } from './dto/updateTrack.dto';
-import { Track } from './track.interface';
+import { Track as TrackModel } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class TrackService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async getAllTracks(): Promise<Track[]> {
+  async getAllTracks(): Promise<TrackModel[]> {
     return await this.databaseService.track.findMany();
   }
 
-  async getTrackById(id: string): Promise<Track> {
+  async getTrackById(id: string): Promise<TrackModel> {
     const track = await this.databaseService.track.findUnique({
       where: { id },
     });
@@ -24,30 +25,34 @@ export class TrackService {
     return track;
   }
 
-  async createTrack(dto: CreateTrackDto): Promise<Track> {
-    return (await this.databaseService.track.create(dto)) as unknown as Track;
-  }
-
-  async deleteTrack(id: string): Promise<Track> {
-    const track = await this.databaseService.track.delete({ where: { id } });
-
-    if (!track) {
-      throw new NotFoundException(`Track with id: ${id} not found`);
-    }
-
-    return track;
-  }
-
-  async updateTrack(id: string, dto: UpdateTrackDto): Promise<Track> {
-    const track = await this.databaseService.track.update({
-      where: { id },
+  async createTrack(dto: CreateTrackDto): Promise<TrackModel> {
+    return await this.databaseService.track.create({
       data: dto,
     });
+  }
 
-    if (!track) {
-      throw new NotFoundException(`Track with id: ${id} not found`);
+  async deleteTrack(id: string): Promise<TrackModel> {
+    try {
+      return await this.databaseService.track.delete({ where: { id } });
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError && e.code === 'P2025')
+        throw new NotFoundException(`Track with id: ${id} not found`);
+
+      throw e;
     }
+  }
 
-    return track;
+  async updateTrack(id: string, dto: UpdateTrackDto): Promise<TrackModel> {
+    try {
+      return await this.databaseService.track.update({
+        where: { id },
+        data: dto,
+      });
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError && e.code === 'P2025')
+        throw new NotFoundException(`Track with id: ${id} not found`);
+
+      throw e;
+    }
   }
 }
